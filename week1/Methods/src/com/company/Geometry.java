e6346294a0524b096d191c759062e3dbadf902d9
Helder Pereira <phelder@gmail.com>
2021-12-15 11:11:39 +0000

    upload

--- week1/Methods/src/com/company/Geometry.java
+++ week1/Methods/src/com/company/Geometry.java
@@ -10,10 +10,47 @@ public class Geometry {
         // 2 - Calculate the area of a rectangle
         // 3 - Calculate the perimeter of a circle (circumference)
         // 4 - Calculate the area of a circle
+
+        rectanglePerimeter(10, 10);
+        rectangleArea(10, 10);
+        circlePerimeter(5);
+        circleArea(5);
+        squarePerimeter(5);
+        squareArea(5);
     }
 
-    // arguments are missing...
-    public static void rectanglePerimeter() {
+    public static void rectanglePerimeter(int width, int height) {
+//        int result = width + width + height + height;
+//        int result = width * 2 + height * 2;
+        int result = (width + height) * 2;
 
+        System.out.println("The perimeter is " + result);
+    }
+
+    public static void rectangleArea(int width, int height) {
+        int result = width * height;
+
+        System.out.println("The area is " + result);
+    }
+
+    public static void circlePerimeter(int radius) {
+        double result = radius * 2 * Math.PI;
+
+        System.out.println("The circumference is " + result);
+    }
+
+    public static void circleArea(int radius) {
+//        double result = Math.pow(radius, 2) * Math.PI;
+        double result = radius * radius * Math.PI;
+
+        System.out.println("The area is " + result);
+    }
+
+    public static void squarePerimeter(int side) {
+        rectanglePerimeter(side, side);
+    }
+
+    public static void squareArea(int side) {
+        rectangleArea(side, side);
     }
 }
---
+++ week1/Methods/src/com/company/Geometry2.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Geometry2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What's the width of the rectangle?");
+        int width = input.nextInt();
+
+        System.out.println("What's the height of the rectangle?");
+        int height = input.nextInt();
+
+        int perimeter = rectanglePerimeter(width, height);
+
+        System.out.printf("The perimeter of your %dx%d rectangle is %d\n",
+                width, height, perimeter);
+    }
+
+    public static int rectanglePerimeter(int width, int height) {
+        return (width + height) * 2;
+    }
+
+    public static int rectangleArea(int width, int height) {
+        return width * height;
+    }
+
+    public static double circlePerimeter(int radius) {
+        return radius * 2 * Math.PI;
+    }
+
+    public static double circleArea(int radius) {
+        return radius * radius * Math.PI;
+    }
+
+    public static int squarePerimeter(int side) {
+        return rectanglePerimeter(side, side);
+    }
+
+    public static int squareArea(int side) {
+        return rectangleArea(side, side);
+    }
+}
