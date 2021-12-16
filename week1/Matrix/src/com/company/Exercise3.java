fe0e032b8f67d087298fc281251999edf3699c4e
Helder Pereira <phelder@gmail.com>
2021-12-16 10:09:17 +0000

    exercise 3

---
+++ week1/Matrix/src/com/company/Exercise3.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Exercise3 {
+    // EXERCISE 3
+    // With a matrix of Random Numbers
+    // Sum and display the value of each row
+    // 2  5  3  2 = 12
+    // 1  5  3  2 = 11
+    // 2  1  3  1 = 7
+    // 2  5  1  2 = 10
+
+    // BONUS
+    // Add + signs between the numbers
+    // 2  + 5  + 3  +  2 = 12
+    // 1  + 5  + 3  +  2 = 11
+    // 2  + 1  + 3  +  1 = 7
+    // 2  + 5  + 1  +  2 = 10
+
+    // BONUS 2
+    // Sum the columns as well
+    // 2  + 5  + 3  +  2 = 12
+    // 1  + 5  + 3  +  2 = 11
+    // 2  + 1  + 3  +  1 = 7
+    // 2  + 5  + 1  +  2 = 10
+    // --------------------
+    // 7   16  10      7
+
+
+    public static void main(String[] args) {
+
+    }
+}
